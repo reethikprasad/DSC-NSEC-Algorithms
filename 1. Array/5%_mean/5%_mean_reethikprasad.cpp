982fd72d1e9d5cff4d0d30bf8f13e71a633312af
Reethik Prasad <reethikprasad@gmail.com>
2021-01-04 12:45:19 +0530

    added 5 % mean

---
+++ 1. Array/5%_mean/5%_mean_reethikprasad.cpp
@@ -0,0 +1,42 @@
+/*
+ * Description
+ * 5%_mean_reethikprasad.cpp
+ * We have to find the  the mean of the  intgers after removing the smallest 
+ * 5% and largest 5% of the elements.
+* First take the number of test cases "t"
+ * Then take the number as an array input with size n
+ * Store them in an array a[n]
+ * Now using the STL sort them in increasing order i.e sort(a,a+n)
+ * After sorting we remove the smallest and the largest 5% array
+ * Store the remaining in total left
+ * Find the mean and print out
+ * time complexity :- O(nlogn)  space complexity :- O(1)
+ * @author [reethikprasad](https://github.com/reethikprasad)
+ * 
+ * **/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    int a[n];
+    int five=(n*5)/100;
+    float sum=0,mean;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++)
+        if(i>=five && i<(n-five))
+        sum=sum+a[i];
+    int totalleft=n-2*five;
+    mean=sum/(totalleft);
+    cout<<fixed;
+    cout<<setprecision(5)<<mean<<endl;
+    }
+    return 0;
+}
